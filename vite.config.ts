8a25854e5d331732f881ebbe2f3c6cca5f0f0ba7
dhruvchoduhary-ux <tufdhruv@gmail.com>
2026-06-16 21:58:05 +0530

    Test build without singlefile plugin

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   base: '/digital-agency-website-development/', // 👈 This line tells GitHub where your site lives
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
